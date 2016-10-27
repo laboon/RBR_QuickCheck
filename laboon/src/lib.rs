46f19e527d59897d7bb62b4b9cf899a92b5d971b
laboon <laboon@gmail.com>
2016-10-27 10:08:22 -0400

    add framework for ex

---
+++ laboon/src/lib.rs
@@ -0,0 +1,18 @@
+extern crate quickcheck;
+
+// Add laboon function here
+
+#[cfg(test)]
+
+mod tests {
+
+    // Uncomment this once laboon function created
+    
+    // use super::laboon;
+
+    // QuickCheck imports
+    
+    use quickcheck::quickcheck;
+    use quickcheck::TestResult;
+
+}
